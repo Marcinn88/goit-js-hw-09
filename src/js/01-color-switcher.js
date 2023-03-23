5c78e933afb3a223a47faaabf8e5faa956fd5509
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-03-23 17:39:31 +0100

    random color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
-//colors
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startButton = document.querySelector(`body button[data-start]`);
+const stopButton = document.querySelector(`body button[data-stop]`);
+const body = document.querySelector('body');
+const colorChanger2 = () => {
+  console.log(stopButton);
+};
+
+stopButton.disabled = true;
+
+const colorChanger = () => {
+  let backColor = getRandomHexColor();
+  body.style.backgroundColor = backColor;
+  startButton.disabled = true;
+  stopButton.disabled = false;
+};
+
+startButton.addEventListener('click', () => {
+  colorChanger();
+  timeChanger = setInterval(() => {
+    let backColor = getRandomHexColor();
+    body.style.backgroundColor = backColor;
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timeChanger);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
